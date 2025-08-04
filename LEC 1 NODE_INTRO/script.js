0fc15ff3f528e57e877581d22ffe7504bd9c8cb2
ayushsahu01 <ayushsahu29042005@gmail.com>
2025-08-04 11:28:17 +0530

    Lecture-1 Jovac

---
+++ LEC 1 NODE_INTRO/script.js
@@ -0,0 +1,97 @@
+// var figlet = require("figlet");
+const fs = require('fs')
+
+
+// figlet("Gourvi", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
+
+
+
+//   readFile
+// fs.readFile("math.txt","utf8",(err, data)=>{
+//     if(err){
+//         console.log("error found in your code :",err);
+        
+//     }
+//     console.log(data);
+    
+// })
+
+
+// writeFile
+
+// fs.writeFile("text.txt","hello bachhoooooooooo",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("file was created successfully");
+//     }
+// })
+
+
+
+
+// update 
+// fs.appendFile("text.txt","hello students",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("file was updated successfully");
+//     }
+// })
+
+// delete file
+// fs.unlink("math.txt",(err)=>{
+//     if(err){
+//         console.log(err);
+//     }
+//     console.log("file deleted successfully");
+    
+// })
+
+
+// remane
+// fs.rename("app.js","server.js",(err)=>{
+//     if(err){
+//         console.log(err);
+        
+//     }
+//     console.log("yes file ka name change ho gya");
+    
+// })
+
+
+// create folder
+// fs.mkdir("newFolder",(err)=>{
+//     if(err){
+//         console.log(err);
+//         return
+//     }
+//     console.log("folder created");
+    
+// })
+
+
+//READ FOLDER
+// fs.readdir("huhuhu",(err,file)=>{
+//     if(err){
+//         console.log(err);
+//         return
+//     }
+//     console.log("folder read :", file);
+    
+// })
+
+// fs.rmdir("newFolder",{ recursive : true },(err)=>{
+//     if(err){
+//         console.log("folder path not found :",err);
+//     }else{
+//         console.log("folder deleted successfully"); 
+//     }
+// })
